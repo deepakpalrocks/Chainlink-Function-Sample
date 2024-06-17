8c2e717249488f190a666567783bf8e659510d86
Deepak Pal <85776383+deepakpalrocks@users.noreply.github.com>
2024-06-17 18:02:38 +0530

    Add files via upload

---
+++ functions-examples/examples/2-call-api/request.js
@@ -0,0 +1,134 @@
+const fs = require("fs");
+const path = require("path");
+const {
+  SubscriptionManager,
+  simulateScript,
+  ResponseListener,
+  ReturnType,
+  decodeResult,
+  FulfillmentCode,
+} = require("@chainlink/functions-toolkit");
+const functionsConsumerAbi = require("../../abi/functionsClient.json");
+const ethers = require("ethers");
+require("@chainlink/env-enc").config();
+
+const consumerAddress = "0x4615aebbcbdc342cd5cce5246e25141fdd80fec5"; // REPLACE this with your Functions consumer address
+const subscriptionId = 3114; // REPLACE this with your subscription ID
+
+const makeRequestSepolia = async () => {
+  // hardcoded for Ethereum Sepolia
+  const routerAddress = "0xb83E47C2bC239B3bf370bc41e1459A34b41238D0";
+  const linkTokenAddress = "0x779877A7B0D9E8603169DdbD7836e478b4624789";
+  const donId = "fun-ethereum-sepolia-1";
+  const explorerUrl = "https://sepolia.etherscan.io";
+
+  // Initialize functions settings
+  const source = fs
+    .readFileSync(path.resolve(__dirname, "source.js"))
+    .toString();
+
+  const args = ['59b3dffb89b4ee5476d928638cac4e8450212d1088ef269f6ef565ce4ca378da', `bc1qs0cc02edpyzp8np59l92jav4pql5xv4axutg3n`];
+  const gasLimit = 300000;
+
+  // Initialize ethers signer and provider to interact with the contracts onchain
+  const privateKey = process.env.PRIVATE_KEY; // fetch PRIVATE_KEY
+  if (!privateKey)
+    throw new Error(
+      "private key not provided - check your environment variables"
+    );
+
+  const rpcUrl = process.env.ETHEREUM_SEPOLIA_RPC_URL; // fetch Sepolia RPC URL
+
+  if (!rpcUrl)
+    throw new Error(`rpcUrl not provided  - check your environment variables`);
+
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+  const wallet = new ethers.Wallet(privateKey);
+  const signer = wallet.connect(provider); // create ethers signer for signing transactions
+
+  //////// MAKE REQUEST ////////
+  console.log("\nMake request...");
+
+  const functionsConsumer = new ethers.Contract(
+    consumerAddress,
+    functionsConsumerAbi,
+    signer
+  );
+
+  // Actual transaction call
+  const transaction = await functionsConsumer.sendRequest(
+    source, // source
+    "0x", // user hosted secrets - encryptedSecretsUrls - empty in this example
+    0, // don hosted secrets - slot ID - empty in this example
+    0, // don hosted secrets - version - empty in this example
+    args,
+    [], // bytesArgs - arguments can be encoded off-chain to bytes.
+    subscriptionId,
+    gasLimit,
+    ethers.utils.formatBytes32String(donId) // jobId is bytes32 representation of donId
+  );
+
+  // Log transaction details
+  console.log(
+    `\n✅ Functions request sent! Transaction hash ${transaction.hash}. Waiting for a response...`
+  );
+
+  console.log(
+    `See your request in the explorer ${explorerUrl}/tx/${transaction.hash}`
+  );
+
+  const responseListener = new ResponseListener({
+    provider: provider,
+    functionsRouterAddress: routerAddress,
+  }); // Instantiate a ResponseListener object to wait for fulfillment.
+  (async () => {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        responseListener
+          .listenForResponseFromTransaction(transaction.hash)
+          .then((response) => {
+            resolve(response); // Resolves once the request has been fulfilled.
+          })
+          .catch((error) => {
+            reject(error); // Indicate that an error occurred while waiting for fulfillment.
+          });
+      });
+
+      const fulfillmentCode = response.fulfillmentCode;
+
+      if (fulfillmentCode === FulfillmentCode.FULFILLED) {
+        console.log(`\n✅ Request ${ response.requestId } successfully fulfilled`);
+      } else if (fulfillmentCode === FulfillmentCode.USER_CALLBACK_ERROR) {
+        console.log(`\n⚠️ Request ${response.requestId} fulfilled. However, the consumer contract callback failed`);
+      } else {
+        console.log(`\n❌ Request ${response.requestId} not fulfilled.`);
+      }
+
+      const errorString = response.errorString;
+      if (errorString) {
+        console.log(`\n❌ Error during the execution: `, errorString);
+      } else {
+        const responseBytesHexstring = response.responseBytesHexstring;
+        if (ethers.utils.arrayify(responseBytesHexstring).length > 0) {
+          const userTransferredBTC = decodeResult(
+            response.responseBytesHexstring,
+            ReturnType.uint256
+          );
+          console.log(
+            `\n✅ Amounts of satoshi sent to magpieCustodian wallet: ${ReturnType.uint256}: `,
+            userTransferredBTC
+          );
+          // const userETHAddress = await userMappingContract.getUserEthAddress(userBTCAddress);
+          // await functionsConsumer.mintTo(userETHAddress, userTransferredBTC);
+        }
+      }
+    } catch (error) {
+      console.error("Error listening for response:", error);
+    }
+  })();
+};
+
+makeRequestSepolia().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
---
+++ functions-examples/examples/2-call-api/source.js
@@ -0,0 +1,35 @@
+// This example shows how to make a call to an open API (no authentication required)
+// to retrieve asset price from a symbol(e.g., ETH) to another symbol (e.g., USD)
+
+// CryptoCompare API https://min-api.cryptocompare.com/documentation?key=Price&cat=multipleSymbolsFullPriceEndpoint
+
+// Refer to https://github.com/smartcontractkit/functions-hardhat-starter-kit#javascript-code
+
+// Arguments can be provided when a request is initated on-chain and used in the request source code as shown below
+const txnHash = args[0];
+console.log("hash:", txnHash);
+const magpieCustodianWallet = args[1];
+// const magpieCustodianWallet = `bc1p6eklcpk4tzuvc5wkjm0v770jx6jvwqz8s56mypjz74tggzcr4aeqc5y0xj`
+
+// make HTTP request
+const url = `https://blockchain.info/rawtx/${txnHash}?format=json`;
+console.log(url);
+
+const txnInfo = Functions.makeHttpRequest({
+  url: url,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+const txnData = await txnInfo;
+const transaction = txnData.data;
+let transferredBTC = 0;
+transaction.out.forEach(output => {
+  if (output.addr === magpieCustodianWallet) {
+    const amountBTC = output.value / 100000000; // Convert from satoshis to BTC
+    transferredBTC = output.value;
+    console.log(`Amount transferred to ${magpieCustodianWallet}: ${amountBTC} BTC`);
+  }
+});
+
+return Functions.encodeUint256(transferredBTC);
